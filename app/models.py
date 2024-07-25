a082a004bf1f8d2fc8c01766cc93f8f7cd8bce47
lyan muriithi <muriithilyan1gmail.comgit config --global user.name lyangit config --global user.email muriithilyan1gmail.com>
2024-07-25 21:35:35 +0300

    drop tables

--- app/models.py
+++ app/models.py
@@ -89,4 +89,9 @@ class SalesOut(SalesModel):
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
+def drop_tables():
+    Base.metadata.drop_all(bind=engine)
+
+drop_tables()
 create_tables()
+
